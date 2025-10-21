c4b2ec5516dc674fe58d15d53f38dd6022775077
Chandra Kireeti Kotturu <104622340+kireetikotturu@users.noreply.github.com>
2025-10-21 11:16:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let listElement = document.querySelectorAll("ul li");
+
+let count = 0
+
+listElement.forEach((s, i)=>{
+	let getA = s.getAttribute("id");
+  if(getA === "level"){
+    alert(`The level of the element is: ${i}`)
+  }
+})
